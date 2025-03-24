7f9b37ccb9fd5af4837b1fd134f3e0727f8d132e
Cody Hookey <codyhookey@outlook.com>
2025-03-24 19:34:12 +0000

    update to fix

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
       </nav>
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/movie/:id" component={movie} />
+        <Route path="/movie/:id" component={Movie} />
         <Route component={NotFound} />
       </Switch>
     </Router>
